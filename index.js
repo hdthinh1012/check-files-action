b735133d2d7d1f76c8293b7322b68ba0bca1c38e
hdthinh1012 <hdthinh01@gmail.com>
2023-04-15 03:38:32 +0700

    Check file add

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const core = require('@actions/core');
 
 const github = require('@actions/github');
 
+const fs = require('fs');
+
+async function checkFileExists(filePath) {
+    return fs.promises.access(filePath).then(
+        () => {
+            core.info(`File ${filePath} exists`);
+            return true;
+        },
+    )
+        .catch(() => {
+            core.setFailed(`File ${filePath} does not exist`);
+            return false
+        })
+}
+
 (
     async () => {
         try {
+            checkFileExists("LICENSE");
+            checkFileExists("README.md");
             core.notice('Hello World!');
         }
         catch (error) {
